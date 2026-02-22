75b7dce4aa207fc2b8fc5a9b73eedf7bad4a3ac3
Norhan Gamal <norhan25gamal@gmail.com>
2026-02-22 15:39:04 +0200

    Scrolled Navbar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/layout/Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from '../components/layout/Footer'
 
 export default function MainLayout() {
+    const [scrolled, setScrolled] = useState(false);
+        useEffect(() => {
+    const handleScroll = () => {
+        if (window.scrollY > 50) {
+        setScrolled(true);
+        } else {
+        setScrolled(false);
+        }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
 return (
     <div>
-            <nav className={`fixed top-0 left-0 flex justify-between min-h-[80px] items-center z-50 w-full py-2`} >
-
+        <nav className={`fixed top-0 left-0 flex justify-between min-h-[80px] items-center z-50 w-full py-2 ${scrolled ? "bg-white shadow-md" : "bg-transparent"}`} >
         <Navbar/>
         </nav>
     <main>
