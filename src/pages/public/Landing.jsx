f35a15a8adef1c7ca3c7e787670d82228a232673
search <samaatarek12z@gmail.com>
2026-02-22 17:37:06 +0200

    edit in styling

--- src/pages/public/Landing.jsx
+++ src/pages/public/Landing.jsx
@@ -3,18 +3,21 @@ import { FaChevronCircleRight } from "react-icons/fa";
 
 export default function Landing() {
   return (
-    <div className="w-full">
+    <div className="w-full min-h-screen flex flex-col">
       {/* الصورة */}
-      <div className="w-full">
+      <div className="w-full relative overflow-hidden h-[900px]  ">
         <img
           src="/landing.png"
           alt="Landing"
-          className="w-full h-auto object-cover"
+          className="absolute top-1/2 left-1/2 
+-translate-x-1/2 -translate-y-1/2 
+ object-cover my-20 h-full  "
         />
       </div>
 
       {/* السكشن */}
-      <div className="flex flex-col lg:flex-row justify-around items-center px-4 mt-10 gap-10">
+      <div className="flex flex-col lg:flex-row justify-around items-center px-4 mt-10 gap-10 "
+>
         {/* العمود الأول */}
         <div className="w-[320px] bg-gray-400 rounded-3 p-5">
           <h1 className="text-2xl font-bold mb-4">لماذا تختار خدمتك ؟</h1>
@@ -43,8 +46,8 @@ export default function Landing() {
         </div>
 
 
-        <div className="w-[320px]">
-          <img src="/landinggg.png" className="w-full h-auto" alt="Frame" />
+        <div className="w-[490px] h-full">
+          <img src="/landinggg.png" className="w-full h-full object-cover" alt="Frame" />
         </div>
 
       
