f59c6dbfb600ac48339e0abbc0468a37b3c568fa
Norhan Gamal <norhan25gamal@gmail.com>
2026-03-05 07:04:06 +0200

    Refactor CraftmanDetails component
    Removed order handling logic and updated rating display. Adjusted services and availability props.

--- src/pages/public/CraftmanDetails/CraftmanDetails.jsx
+++ src/pages/public/CraftmanDetails/CraftmanDetails.jsx
@@ -7,7 +7,6 @@ import { FaStar } from "react-icons/fa";
 import { useParams } from "react-router-dom";
 
 function CraftmanDetails() {
-    // Craftsmen Data
     const [craftman, setCraftman] = useState(null);
     const { id } = useParams();
     useEffect(() => {
@@ -16,42 +15,7 @@ function CraftmanDetails() {
             .then(data => setCraftman(data))
             .catch(err => console.log(err));
     }, [id]);
-    // OrderBtn
-    const handleOrder = async () => {
-        if (!craftman) return;
-
-        try {
-            const token = localStorage.getItem("token");
-            if (!token) {
-                alert("يجب تسجيل الدخول أولاً");
-                return;
-            }
-
-            const response = await fetch("https://herafie.runasp.net/api/Orders", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${token}`
-                },
-                body: JSON.stringify({
-                    craftsmanId: craftman.id,
-                    serviceId: 0,
-                    description: "أرغب في الحجز",
-                    scheduledAt: new Date().toISOString()
-                })
-            });
-
-            if (!response.ok) throw new Error("فشل إرسال الطلب");
-
-            const data = await response.json();
-            console.log(data);
-            alert("تم إرسال الطلب بنجاح ");
-
-        } catch (error) {
-            console.error(error);
-            alert("حدث خطأ أثناء إرسال الطلب");
-        }
-    };
+    const prices = craftman?.services.map(service => service.price);
     if (!craftman) return <p className='min-h-screen py-10 px-4 mt-20 text-center font-bold text-3xl'>
         <span className='text-red-500'>...</span> Loading </p>
     return (
@@ -59,15 +23,14 @@ function CraftmanDetails() {
             <div className="min-h-screen bg-gray-100 py-10 px-4 mt-20">
                 <div className="max-w-2xl mx-auto bg-white rounded-3xl shadow-xl overflow-hidden border border-gray-100">
 
-                    {/* Header Section */}
                     <div className="bg-secondary p-8 relative flex  items-center justify-between text-white ">
                         <div className="z-10 ">
                             <h1 className="text-2xl font-bold ">{craftman.fullName}</h1>
                             <p className="text-gray-300"> الخبره: {craftman.yearsOfExperience} <br />{craftman.bio}</p>
                             <div className="flex items-center gap-1 mt-1 text-yellow-400">
-                                <span className='flex gap-2 items-center'> تقييم :{[...Array(craftman.rating)].map((_, index) => (
-                                    <FaStar key={index} />
-                                ))}</span>
+                                <span className='flex gap-2 items-center'> تقييم :{craftman.rating}
+                                    <FaStar />
+                                </span>
                             </div>
                         </div>
                         <div className="absolute top-0 left-0 w-40 h-50 overflow-hidden">
@@ -83,14 +46,12 @@ function CraftmanDetails() {
                             </p>
                         </section>
 
-                        {/* الخدمات المقدمة */}
                         <section className="bg-white p-4 rounded-xl border border-gray-50 shadow-sm">
-                            <OurServices />
+                            <OurServices services={craftman.services} />
                         </section>
-                        {/* السعر التقريبي */} {/* المواعيد المتاحة */}
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                             <AvalibaleTimes
-                                availabilities={craftman.availabilities} />
+                                availabilities={craftman.availabilities} prices={prices} />
                         </div>
 
                         <section className='shadow-sm rounded-xl p-4'>
@@ -101,9 +62,8 @@ function CraftmanDetails() {
                         </section>
 
                         <button
-                            onClick={handleOrder}
                             className="w-full bg-[#d35400] hover:bg-[#b34700] text-white
-                         font-bold py-3 rounded-xl transition-all shadow-lg mt-4">
+                            font-bold py-3 rounded-xl transition-all shadow-lg mt-4">
                             راسل الحرفي الآن
                         </button>
                     </div>
