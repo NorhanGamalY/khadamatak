0fa53dddc5898b61eb41ab6c24afeaf0e3e63bcb
Norhan Gamal <norhan25gamal@gmail.com>
2026-03-08 06:15:17 +0200

    Fix link to details page using order.id

--- src/pages/craftsman/EndidRequest.jsx
+++ src/pages/craftsman/EndidRequest.jsx
@@ -54,7 +54,7 @@ function EndidRequest() {
 
             <div className="flex flex-col gap-3 w-full md:w-auto">
               <NavLink
-                to={`/craftsman/requests/details/${item.id}`}
+                to={`/craftsman/requests/details/${order.id}`}
 
                 className="bg-white border border-gray-300 hover:bg-gray-100 text-gray-700 font-medium text-sm px-4 py-2 rounded-md transition"
               >
